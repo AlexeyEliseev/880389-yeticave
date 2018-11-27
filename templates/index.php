a361af5fab28d5815bc1d49f52c7a026e8e92509
AlexeyEliseev <34351680+AlexeyEliseev@users.noreply.github.com>
2018-11-27 20:57:54 +0600

    Проверка пользовательских данных

--- templates/index.php
+++ templates/index.php
@@ -24,12 +24,12 @@
                     <img src="<?=$value['lot__image'];?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$value['lot__category'];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['name'];?></a></h3>
+                    <span class="lot__category"><?=htmlspecialchars($value['lot__category']);?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['name']);?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=form($value['cost']);?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=htmlspecialchars(form($value['cost']));?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
