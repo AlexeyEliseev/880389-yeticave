fb6e38232768ba5952a33a8c5765d2a6664f8334
Alex <34351680+AlexeyEliseev@users.noreply.github.com>
2018-12-03 01:46:06 +0600

    База данных

--- schema.sql
+++ schema.sql
@@ -5,9 +5,18 @@ CREATE DATABASE yeticave
 USE yeticave;
 
 CREATE TABLE users (
-	id		  INT AUTO_INCREMENT PRIMARY KEY,
-	email     CHAR(128) NOT NULL UNIQUE,
-	password  CHAR(64) NOT NULL UNIQUE
+	id		  			INT AUTO_INCREMENT PRIMARY KEY,
+	registrationdate
+	email     			CHAR(128) NOT NULL UNIQUE,
+	password  			CHAR(64) NOT NULL,
+	avatar				CHAR(128),
+	info				CHAR(128),
+
+	lots_id				INT,
+	rates_id			INT,
+
+	FOREIGN KEY (lots_id) REFERENCES lots(id),
+	FOREIGN KEY (rates_id) REFERENCES rates(id)
 );
 
 CREATE TABLE categories (
@@ -16,9 +25,31 @@ CREATE TABLE categories (
 );
 
 CREATE TABLE lots (
-	id             INT AUTO_INCREMENT PRIMARY KEY, 
-	name           CHAR(128),
+	id             INT AUTO_INCREMENT PRIMARY KEY,
+	createdate     DATE,
+	name           CHAR(64),
+	description    CHAR(128),
+	image		   CHAR(128),
 	cost           INT,
+	enddate		   DATE,
+	ratestep 	   INT,
+
+	user_id 	   INT,
+	winner_id	   INT,
 	categorie_id   INT,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (winner_id) REFERENCES users(id)
 	FOREIGN KEY (categorie_id) REFERENCES categories(id)      
+);
+
+CREATE TABLE rates (
+	id             INT AUTO_INCREMENT PRIMARY KEY,
+	ratedate       DATE, 
+	ratecost       INT,
+
+	user_id   	   INT,
+	lot_id		   INT,
+
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (lot_id) REFERENCES lots(id)      
 );
